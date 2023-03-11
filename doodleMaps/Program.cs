edcfca6cf94814678f5bae53600449c86955a63c
Varvara Chornomorets <vchornomorets@kse.org.ua>
2023-03-11 21:10:25 +0100

    added function which can print path

--- doodleMaps/Program.cs
+++ doodleMaps/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Xml;
-using doodleMaps;
+﻿using doodleMaps;
 
 var generator = new MapGenerator(new MapGeneratorOptions()
 {
@@ -8,61 +7,18 @@ var generator = new MapGenerator(new MapGeneratorOptions()
     Seed = 12 ,
 });
 
-Point Move(Point point, int columnMove, int rowMove)
-{
-    return new Point(point.Column + columnMove, point.Row + rowMove);
-}
-
-List<Point> GetShortestPath(string[,] map, Point start, Point goal)
-{
-    Point previousStep = start;
-    Point position = start;
-    List<Point> path = new List<Point>();
-    path.Add(position);
-    while (position.Column != goal.Column && position.Column != goal.Column)
-    {
-        List<Point> points = new List<Point>();
-        points.Add(Move(position, 1, 0));
-        points.Add(Move(position, -1, 0));
-        points.Add(Move(position, 0, 1));
-        points.Add(Move(position, 0, -1));
-        List<Point> toRemove = new List<Point>();
-        points.Remove(previousStep);
-        foreach (var point in points)
-        {
-            if (0 <= point.Column && point.Column <= 20 && 0 <= point.Row && point.Row <= 20)
-            {
-                if (map[point.Column, point.Row] == "█")
-                    toRemove.Add(point);
-            }
-            else
-                toRemove.Add(point);
-        }
-
-        foreach (var point in toRemove)
-            points.Remove(point);
-        if (points.Count == 1)
-        {
-            path.Add(points[0]);
-            previousStep = position;
-            position = points[0];
-        }
-        else
-        {
-            //тут має бути все інше але я всьо
-        }
-    }
-    return path;
-}
-
-
-
-
-
-
 string[,] map = generator.Generate();
 new MapPrinter().Print(map);
-foreach (var point in GetShortestPath(map, new Point(0, 0), new Point(6, 12)))
+
+void DrawPath(string[,] myMap, List<Point> path)
 {
-    Console.WriteLine(point.Column +" " + point.Row);
+    Point startPoint = path[0];
+    myMap[startPoint.Row, startPoint.Column] = "A";
+    for (int i = 1; i< path.Count-1; i++)
+    {
+        Point point = path[i];
+        myMap[point.Row, point.Column] = ".";
+    }
+    Point endPoint = path[path.Count - 1];
+    myMap[endPoint.Row, endPoint.Column] = "B";
 }
