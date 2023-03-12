118bce96c7e66412808d99b3a135d97313f7a21b
Varvara Chornomorets <vchornomorets@kse.org.ua>
2023-03-12 16:56:40 +0100

    bfs, but there is some confusion with coordinates

--- doodleMaps/Program.cs
+++ doodleMaps/Program.cs
@@ -8,7 +8,6 @@ var generator = new MapGenerator(new MapGeneratorOptions()
 });
 
 string[,] map = generator.Generate();
-new MapPrinter().Print(map);
 
 void DrawPath(string[,] myMap, List<Point> path)
 {
@@ -22,3 +21,92 @@ void DrawPath(string[,] myMap, List<Point> path)
     Point endPoint = path[path.Count - 1];
     myMap[endPoint.Row, endPoint.Column] = "B";
 }
+
+bool IsEqual(Point a, Point b)
+{
+    return (a.Column == b.Column && a.Row == b.Row);
+}
+
+List<Point> GetNeighbours(Point mainPoint)
+{
+    List<Point> neighbours = new List<Point>();
+    int x = mainPoint.Column;
+    int y = mainPoint.Row;
+    List<Point> potentialNeighbours = new List<Point>
+    {
+        new Point(x + 1, y),
+        new Point(x - 1, y),
+        new Point(x, y + 1),
+        new Point(x, y - 1)
+    };
+    foreach (var varPotentialNeighbour in potentialNeighbours)
+    {
+        // if it is in the map AND it is not wall
+        if (0 <= varPotentialNeighbour.Column && varPotentialNeighbour.Column < map.GetLength(0) &&
+            0 <= varPotentialNeighbour.Row && varPotentialNeighbour.Row < map.GetLength(1) &&
+            map[varPotentialNeighbour.Column, varPotentialNeighbour.Row] != "█")
+        {
+            neighbours.Add(varPotentialNeighbour);
+        }
+    
+    }
+
+    return neighbours;
+}
+
+List<Point> SimpleBFS(string[,] myMap, Point start, Point end)
+{
+    Queue<Point> nextPoints = new Queue<Point>();
+    nextPoints.Enqueue(start);
+    List<Point> checkedPoints = new List<Point>();
+    var origins = new Dictionary<Point, Point?>();
+    origins[start] = null;
+    while (nextPoints.Count > 0)
+    {
+        // take first point
+        Point currentPoint = nextPoints.Dequeue();
+        // check if it is finish
+        if (IsEqual(currentPoint, end))
+        {
+            break;
+        }
+        // add point to the list of checked Points
+        checkedPoints.Add(currentPoint);
+        // add its non-checked neighbours to the queue
+        List<Point> currentNeighbours = GetNeighbours(currentPoint);
+        foreach (var neighbour in currentNeighbours)
+        {
+            if (!checkedPoints.Contains(neighbour))
+            {
+                nextPoints.Enqueue(neighbour);
+                checkedPoints.Add(neighbour);
+                // add this points to the dictionary, their parent is currentPoint
+                origins[neighbour] = currentPoint;
+            }
+        }
+    }
+
+    List<Point> result = new List<Point>();
+    result.Add(end);
+    Point currentValue = end;
+    while (!IsEqual(currentValue, start))
+    {
+        var currentParent = origins[currentValue];
+        result.Add(currentParent.Value);
+        currentValue = currentParent.Value;
+
+
+    }
+
+    result.Reverse();
+    return result;
+}
+// List<Point> myPath = SimpleBFS(map, new Point(0,0), new Point(0,18) );
+// DrawPath(map, myPath);
+List<Point> somePath = new List<Point>
+{
+    new Point(0, 0),
+    new Point(0, 1)
+};
+DrawPath(map, somePath);
+new MapPrinter().Print(map);
